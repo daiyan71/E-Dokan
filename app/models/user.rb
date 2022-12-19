0bb4ae308078f1ba0679a526af522b156dd293cf
daiyan71 <daiyanibrahim12@gmail.com>
2022-12-19 21:19:14 +0600

    user setup using devise

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  mount_uploader :image, AvatarUploader
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :contact_number, :user_name, presence: true
+end
